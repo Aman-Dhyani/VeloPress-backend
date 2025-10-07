4ddbcd9ff823ebd76f6a47ce443d4793073d7d69
Aman Dhyani <102688642+Aman-Dhyani@users.noreply.github.com>
2025-10-07 13:54:29 +0530

    Update critical.js

--- critical/critical.js
+++ critical/critical.js
@@ -7,7 +7,7 @@ export async function generateCriticalCss(urls) {
     const results = [];
     const browser = await puppeteer.launch({
       headless: true,
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+      // executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
       args: ["--disable-gpu", "--disable-dev-shm-usage"],
     });
 
