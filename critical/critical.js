7d05507ab3aa552aa463c731009b1609159c88f0
Aman Dhyani <102688642+Aman-Dhyani@users.noreply.github.com>
2025-10-07 11:33:40 +0530

    Update critical.js

--- critical/critical.js
+++ critical/critical.js
@@ -5,16 +5,17 @@ import puppeteer from 'puppeteer';
 export async function generateCriticalCss(urls) {
   try {
     const results = [];
-    const browser = await puppeteer.launch({
-      headless: true,
-      executablePath: '/usr/bin/google-chrome-stable',
-      args: [
-        '--no-sandbox',
-        '--disable-gpu',
-        '--disable-dev-shm-usage',
-        '--disable-setuid-sandbox',
-      ]
-    });
+const browser = await puppeteer.launch({
+  headless: true,
+  executablePath: '/usr/bin/google-chrome-stable', // should exist after install
+  args: [
+    '--no-sandbox',
+    '--disable-gpu',
+    '--disable-dev-shm-usage',
+    '--disable-setuid-sandbox',
+  ]
+});
+
 
     // Loop through each URL in the urls array
     for (const url of urls) {
