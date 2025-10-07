830f77d6dcf5e09e8cb3320ced84dc60ca2b108b
Aman Dhyani <102688642+Aman-Dhyani@users.noreply.github.com>
2025-10-07 16:25:19 +0530

    Update critical.js

--- critical/critical.js
+++ critical/critical.js
@@ -1,36 +1,40 @@
+// generateCriticalCss.js
 import chromium from "@sparticuz/chromium";
 import { generate } from 'critical';
 import puppeteer from 'puppeteer';
 
 export async function generateCriticalCss(urls) {
-  const results = [];
-  const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: chromium.path, // use bundled Chromium
-    args: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
-    ignoreDefaultArgs: ['--disable-extensions']
-  });
+  try {
+    const results = [];
+    const browser = await puppeteer.launch({
+      headless: true,
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+      args: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
+    });
 
-  for (const url of urls) {
-    try {
-      const { css } = await generate({
-        inline: false,
-        base: process.cwd(),
-        src: url,
-        width: 1300,
-        height: 900,
-        penthouse: {
-          puppeteer: browser,
-          timeout: 60000,
-        },
-      });
-      results.push({ domain: url, css });
-    } catch (error) {
-      console.log(`Error generating critical CSS for ${url}: ${error}`);
-      results.push({ domain: url, css: "" });
+    // Loop through each URL in the urls array
+    for (const url of urls) {
+      try {
+        const { css } = await generate({
+          inline: false,
+          base: process.cwd(),
+          src: url,
+          width: 1300,
+          height: 900,
+          penthouse: {
+            puppeteer: browser,
+            timeout: 60000,
+          },
+        });
+        results.push({ domain: url, css });
+      } catch (error) {
+        console.log(`Error generating critical CSS for ${url}: ${error}`);
+        results.push({ domain: url, css: "" });
+      }
     }
+    await browser.close();
+    return results;
+  } catch (error) {
+    throw new Error("Error generating critical CSS: " + error.message);
   }
-
-  await browser.close();
-  return results;
 }
